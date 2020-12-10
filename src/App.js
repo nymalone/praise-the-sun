a4afb2024c8538f0ad9c9813373f6ac8a41e7102
Nykolle Malone <malone.nykolle@gmail.com>
2020-12-09 22:51:33 -0300

    eslit and editorconfig done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StatusBar, View, Text} from 'react-native';
+
+const App = () => (
+  <>
+   <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+)
+
+export default App;
